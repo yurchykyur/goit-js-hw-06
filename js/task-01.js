6e63e68ce2b4f26d741bcf02e16891a91349b335
yurchykyur <yurchyk.yur.s@gmail.com>
2023-05-11 22:35:32 +0300

    refactoring js tast-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,62 +1,18 @@
-// Завдання 1
-
 // Напиши скрипт, який:
 
 // Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
 // Для кожного элемента li.item у списку ul#categories,
 // знайде і виведе в консоль текст заголовку елемента(тегу < h2 >)
 // і кількість елементів в категорії(усіх < li >, вкладених в нього).
-// Для виконання цього завдання потрібно використати метод forEach() і властивості навігації по DOM.
-
-// В результаті, в консолі будуть виведені наступні повідомлення.
-
-// Number of categories: 3
-
-// Category: Animals
-// Elements: 4
-
-// Category: Products
-// Elements: 3
-
-// Category: Technologies
-// Elements: 5
 
 const categoriesElem = document.querySelector('#categories');
 
 const countCategories = categoriesElem.querySelectorAll('li.item').length;
-
 console.log(`Number of categories: ${countCategories}`);
 
-const findAnimals =
-  categoriesElem.firstElementChild.querySelector('h2').textContent;
+const categoriesElemItems = categoriesElem.querySelectorAll('li.item');
 
-console.log('Category: ', findAnimals);
-
-const findAnimalsEl =
-  categoriesElem.firstElementChild.querySelectorAll('li').length;
-
-console.log('Elements: ', findAnimalsEl);
-
-const findProducts =
-  categoriesElem.firstElementChild.nextElementSibling.querySelector(
-    'h2'
-  ).textContent;
-
-console.log('Category: ', findProducts);
-
-const findProductsEl =
-  categoriesElem.firstElementChild.nextElementSibling.querySelectorAll(
-    'li'
-  ).length;
-
-console.log('Elements: ', findProductsEl);
-
-const findTechnologies =
-  categoriesElem.lastElementChild.querySelector('h2').textContent;
-
-console.log('Category: ', findTechnologies);
-
-const findTechnologiesEl =
-  categoriesElem.lastElementChild.querySelectorAll('li').length;
-
-console.log('Elements: ', findTechnologiesEl);
+categoriesElemItems.forEach(function callback(element) {
+  console.log(`Category: ${element.querySelector('h2').textContent}`);
+  console.log(`Elements: ${element.querySelectorAll('li').length}`);
+});
