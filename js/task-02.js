65ea6d7777edc98dfe16b7f1914b03d7c4065b92
Yurchyk Yurii <124770215+yurchykyur@users.noreply.github.com>
2023-05-12 09:40:14 +0000

    work on task 02

--- js/task-02.js
+++ js/task-02.js
@@ -29,3 +29,20 @@ const ingredients = [
 // Додасть назву інгредієнта як його текстовий вміст.
 // Додасть елементу клас item.
 // Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
+const ulIngredients = document.querySelector('#ingredients');
+
+const listOfIngredients = [];
+
+ingredients.forEach(ingredient => {
+ const ingredientsItem = document.createElement( 'li');
+ ingredientsItem.textContent = ingredient;
+ ingredientsItem.classList.add('item');
+ listOfIngredients.push(ingredientsItem);
+ console.log(ingredientsItem);
+})
+console.log(listOfIngredients);
+
+ulIngredients.append(...listOfIngredients)
+
+console.log(ulIngredients)
